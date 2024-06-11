3b63c0664b0d7085f9f2ac752d65c91b2e018a56
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-06-11 15:24:19 -0300

    template detalhe produto

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpRequest, HttpResponse
 from .models import Produto
@@ -14,11 +15,11 @@ class ListarProdutos(ListView):
 
 
 
-class DetalheProduto(View):
-    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        return HttpResponse("Detalhar")
-
-
+class DetalheProduto(DetailView):
+    model= Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
