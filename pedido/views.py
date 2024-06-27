7ebf1be3341d5b9621ea6faf546cffb83b532b61
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-06-27 18:40:17 -0300

    inicio processo- salvando pedido

--- pedido/views.py
+++ pedido/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 
 
 class Pagar(View):
-    pass
+    template_name='pedido/pagar.html'
+    def get(self, *args, **kwargs):
+        
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer o login.'
+            )
+            return redirect('perfil:CreateUser')
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio'
+            )
+            return redirect('produto:ListarProdutos')
+        contexto = {
+            
+        }
+        return render(self.request, self.template_name, contexto)
 
+
+class SalvarPedido(View):
+    pass
 
 class FecharPedido(View):
     pass
