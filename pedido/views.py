98efa7ea53f4c7462d3f746ad0fcc2a731659510
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-06-28 13:26:30 -0300

    Update views.py

--- pedido/views.py
+++ pedido/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
+from produto.models import Variacao
 
 
 class Pagar(View):
@@ -21,6 +22,32 @@ class Pagar(View):
                 'Carrinho vazio'
             )
             return redirect('produto:ListarProdutos')
+        carrinho = self.request.session.get('carrinho')
+        carrinho_variacao_ids = [v for v in carrinho]
+        db_variacoes = list(Variacao.objects.select_related('produto').filter(id_in=carrinho_variacao_ids))
+        for variacao in  db_variacoes:
+            vid = str(variacao.id)
+            estoque= variacao.estoque
+            qtd_carrinho = carrinho[vid]['quantidade']
+            preco_unt = carrinho[vid]['preco_unitario']
+            preco_unt_promo = carrinho[vid]['preco_unitario_promocional']
+            
+            error_msg_estoque = ''
+            
+            if estoque < qtd_carrinho:
+                carrinho[vid]['quantidade'] = estoque
+                carrinho[vid]['preco_quantitativo'] = estoque * preco_unt
+                carrinho[vid]['preco_quantitativo_promocional'] = estoque * preco_unt_promo
+                error_msg_estoque = 'Estoque insuficiente para alguns produtos no seu carrinho.'
+                
+            if error_msg_estoque:
+                messages.error(
+                    self.request,
+                    'Estoque insuficiente para alguns produtos no seu carrinho.'
+                )
+                self.request.session.save()
+                return redirect('produto:Carrinho')
+            
         contexto = {
             
         }
